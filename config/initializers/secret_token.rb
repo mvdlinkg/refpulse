463d9bedd82ded3a27b4841f3af62df84b1555fb
German Monfort <german.monfort@gmail.com>
2012-03-07 16:32:10 -0200

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefereePulse::Application.config.secret_token = '6ef813c0536883d6b259b4669bb1d1defc4123916778ab80122246cde324f6519f53e42fb63d87089f996e9e72750cfb54689d5ce3aaf7eb77843b8895cff53a'
