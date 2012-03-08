4d24dc476f0a314773f1df9211a8742045606162
German <monfort.german@gmail.com>
2012-03-07 23:29:25 -0200

    Adding devise views + settingslogic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  # before_filter :authenticate_user!, :except => [:some_action_without_auth]
+  before_filter :load_metadata
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = exception.message
+    redirect_to root_url
+  end
+
+  def load_metadata
+    @title = Settings.site['name']
+    @metatags = Settings.site['metatags']
+    @description = Settings.site['description']
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,7 @@ class User < ActiveRecord::Base
           :validatable,
           :confirmable,
           :lockable,
-          :timeoutable,
-          :encryptable
+          :timeoutable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
