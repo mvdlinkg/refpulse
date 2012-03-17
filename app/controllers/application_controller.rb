953b413f6df33e9a95cdf11d3338ae7c1eaffe92
German <monfort.german@gmail.com>
2012-03-17 01:26:12 -0300

    Adding basic head structure + layout and helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,29 @@ class ApplicationController < ActionController::Base
   # before_filter :authenticate_user!, :except => [:some_action_without_auth]
   before_filter :load_metadata
 
+  if Rails.env.production?
+    rescue_from Exception do |e|
+      render 'errors/exception', :status => 500
+    end
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
+  end
+
+  private
+
+  def store_referer
+    session[:return_to] = request.referer
+  end
+
+  def store_return_to(path)
+    session[:return_to] = path
+  end
+
+  def redirect_back_or_to(default_url)
+    redirect_to session.delete(:return_to) || default_url
   end
 
   def load_metadata
